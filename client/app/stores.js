afc879511525113f95953119d35eb308b7bae629
Donald Harvey <donald@donaldharvey.co.uk>
2017-05-10 19:30:05 +0100

    missing stores file

---
+++ client/app/stores.js
@@ -0,0 +1,5 @@
+import UserStore from './users/store';
+import MessageStore from './messages/store';
+import RecordingStore from './recordings/store';
+
+export {UserStore, MessageStore, RecordingStore};
